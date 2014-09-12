fa53ab2d500fd14933baa981bb817c5caeefb652
Keir Nellyer <keir@nellyer.co.uk>
2014-09-12 21:39:16 +0100

    Initial Commit :)

--- README.md
+++ README.md
@@ -9,67 +9,29 @@ Setup
 Include Messenger in your project and instantiate it when your plugin loads:
 
 ```java
-Messenger messenger;
-
-...
-
 public void onEnable() {
-    messenger = new Messenger(this);
+    Messenger.load(new File(getDataFolder(), "language.properties"))
 }
 ```
 
-By default messenger will create a file in ```plugins/PluginName/messages.yml```. If you wish to change this you may use:
-
-```java
-messenger.setFileName("strings.yml");
-```
-
-Before using messenger to send any values, you must call ```messenger.load();```. Do this ***after*** you have set any defaults and other options you wish to be present.
+The above must be called for modified values to be loaded and default values to be saved to the config. If not called, it will always return default values.
 
 
 Defaults
 -------
 
-Set up defaults for your plugin. Defaults are a map where the key is a String that the data will be stored under in YAML, and the value is either a String OR a String List that is the message itself.
-When String Lists are used, each new String in the list will be sent as a new line.
-
-In order to make retrieving messages easier, you should use constant String values to keep track of your keys, for instance:
-
-```java
-public static final String MESSAGE_NO_PERMISSION = "no-permission";
-public static final String MESSAGE_USAGE = "usage";
-
-Map<String, Object> defaults = new HashMap<String, Object>(){{
-    put(MESSAGE_NO_PERMISSION, "You do not have permission to use that command");
-    put(MESSAGE_USAGE, Arrays.asList(new String[]{"Usage:", "/mycommand <something> [something]"}));
-}};
-```
-
-You can then set the defaults to be used by the Messenger:
-
-```java
-messenger.setDefaults(defaults);
-```
-
-The example above would produce a default messages.yml file containing the following:
-
-```yaml
-no-permission: 'You do not have permission to use that command'
-usage:
-- 'Usage:'
-- '/mycommand <something> [something]'
-```
+Set up defaults for your plugin. Defaults will be saved to the language file if they do not exist when load() is called. See example values in the Messenger class to see how this is done.
 
 Sending
 -------
 
-Before messages are sent, you must call ```messenger.load();```
+Before messages are sent, you must call ```Messenger.load(File);```
 
 Using String constants then allows easy sending of these messages to any CommandSender:
 
 ```java
 CommandSender cs = ...
-messenger.send(MESSAGE_NO_PERMISSION, cs);
+Messenger.NO_PERMISSION.send(cs);
 ```
 
 If you have Objects to use to format the message, you may call it like so:
@@ -78,7 +40,7 @@ If you have Objects to use to format the message, you may call it like so:
 String value1 = ...
 String value2 = ...
 CommandSender cs = ...
-messenger.send(MESSAGE_NO_PERMISSION, cs, value1, value2);
+Messenger.NO_PERMISSION.send(cs, value1, value2);
 ```
 
 Learn more about message formatting in the section below.
@@ -86,13 +48,9 @@ Learn more about message formatting in the section below.
 Formatting
 -------
 
-All messages are sent with the message prefix in front of them. You can change this prefix to your liking:
+Messages can be sent with a prefix in front of them. You can change the prefix by changing the default value of PREFIX in Messenger.
 
-```java
-messenger.setPrefix(ChatColor.GOLD + "[MyPlugin] " + ChatColor.WHITE);
-```
-
-Or you can disable the prefix by setting it to null.
+Or you can disable the prefix by removing it from the enum.
 
 All messages support the use of ```&``` to denote color codes, such as ```&e``` for yellow, where color codes can be found [here](http://ess.khhq.net/mc/).
 
@@ -101,11 +59,13 @@ All messages support the use of [Java's String.format](http://docs.oracle.com/ja
 For example, the following message could be included in the plugin:
 
 ```java
-public static final String MESSAGE_FORMATTED = "message-formatted";
+public enum Messenger {
 
-Map<String, Object> defaults = new HashMap<String, Object>(){{
-    put(MESSAGE_FORMATTED, "Your name is: %s");
-}};
+...
+
+FORMATTED("Your name is: %s"),
+
+...
 ```
 
 The message could then be formatted when a CommandSender sends a command by calling:
@@ -113,7 +73,7 @@ The message could then be formatted when a CommandSender sends a command by call
 ```java
 CommandSender cs = ...
 String name = cs.getName();
-messenger.send(MESSAGE_FORMATTED, cs, name);
+Messenger.FORMATTED.send(cs, name);
 ```
 
 In general, it is good practice to use String variables when formatting so that they can easily be reorganized by a user.
--- src/main/java/com/puzlinc/messenger/Messenger.java
+++ src/main/java/com/puzlinc/messenger/Messenger.java
@@ -20,299 +20,163 @@ package com.puzlinc.messenger;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.conversations.Conversable;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.*;
+import java.util.Properties;
 
-public class Messenger {
+/**
+ * Class to handle language values.
+ * This version achieves very simple language files by making use of enums.
+ * It also uses properties files instead of yaml files for message storing.
+ *
+ * @author Keir Nellyer
+ */
+public enum Messenger {
 
-    // Begin example message keys - edit these if you modify the class to suit your needs
-    public static final String EXAMPLE = "example-string";
-    public static final String EXAMPLE_FORMAT = "example-string-format";
-    public static final String EXAMPLE_COLOR = "example-string-color";
-    public static final String EXAMPLE_LIST = "example-string-list";
-    // End keys
+    // Below are some values you should probably modify
+    PREFIX("&7[&6MyPlugin&7] &f"),
+    EXAMPLE_SIMPLE("This is the default value"),
+    EXAMPLE_FORMATTED("This is a string with some data in it: %s"),
+    EXAMPLE_COLOR("This is a string with some &ccolor in it");
 
-    private Map<String, Object> defaults = new HashMap<String, Object>(){{
-        put(EXAMPLE, "This is a string");
-        put(EXAMPLE_FORMAT, "This is a string with some data in it: %s");
-        put(EXAMPLE_COLOR, "This is a string with some &ccolor in it");
-        put(EXAMPLE_LIST, Arrays.asList(new String[]{"This is the first message", "This is the second message"}));
-    }};
+    // Code after this point shouldn't need to be touched :)
 
-    private String prefix = ChatColor.DARK_GRAY + "[PLUGIN] " + ChatColor.WHITE;
+    private static final String PREFIX_KEY = "PREFIX";
 
-    private String fileName = "messages.yml"; // File to store as inside data folder
+    private final String defaultValue;
+    private String currentValue;
 
-    private static final String SPLIT_TOKEN = "\n";
-
-    private File parentFile;
-    private File configFile;
-    private YamlConfiguration config;
-
-    /**
-     * Create an instance of the Messenger for a given plugin.
-     * Note: {@link Messenger#load()} must be called before values can be retrieved.
-     * @param plugin Your JavaPlugin.
-     */
-    public Messenger(JavaPlugin plugin) {
-        parentFile = plugin.getDataFolder();
-        configFile = new File(parentFile, fileName);
-
-        // Run unique test on keys
-        testDuplicates();
+    private Messenger(String defaultValue){
+        this.defaultValue = defaultValue;
+        this.currentValue = ChatColor.translateAlternateColorCodes('&', defaultValue);
     }
 
     /**
-     * Set the default message values.
-     * @param defaults Map where key is String for YAML storage, value is either String or List<String>.
+     * Returns the default value which will be written to the config if the value doesn't exist.
+     *
+     * @return The default value
      */
-    public void setDefaults(Map<String, Object> defaults) {
-        this.defaults = defaults;
+    public String getDefaultValue() {
+        return defaultValue;
     }
 
     /**
-     * Set the prefix sent before any message.
-     * Set to null for no prefix.
-     * @param prefix Prefix, or null for no prefix.
+     * Gets the current value contained in the language file with a prefix.
+     *
+     * @param args Arguments which should be used when formatting the value, see String#format(String, Object...)
+     * @return The current value, formatted using the provided arguments
      */
-    public void setPrefix(String prefix) {
-        this.prefix = prefix;
+    public String getCurrentValue(Object... args) {
+        return getCurrentValue(true, args);
     }
 
-    /**
-     * The file name inside of the plugin's data folder to save values in.
-     * @param fileName Messages file name.
-     */
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-        configFile = new File(parentFile, fileName);
-    }
+    public String getCurrentValue(boolean prefix, Object... args){
+        String message = String.format(currentValue, args);
 
-    /**
-     * Get all default values.
-     * @return Defaults.
-     */
-    public Map<String, Object> getDefaults() {
-        return defaults;
-    }
-
-    /**
-     * Get message prefix.
-     * @return Prefix.
-     */
-    public String getPrefix() {
-        return prefix;
-    }
-
-    /**
-     * Get file name.
-     * @return File name.
-     */
-    public String getFileName() {
-        return fileName;
-    }
-
-    /**
-     * Load the configuration and create any default files / values as needed.
-     * This function must be called before values can be retrieved, and after defaults or file name changes are made.
-     */
-    public void load() {
-        if (!parentFile.exists()) {
-            parentFile.mkdir();
-        }
-        if (!configFile.exists()) {
+        if (prefix){
             try {
-                configFile.createNewFile();
-                config = YamlConfiguration.loadConfiguration(configFile);
-                for (String key : defaults.keySet()) {
-                    config.set(key, defaults.get(key));
-                }
-                save();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            config = YamlConfiguration.loadConfiguration(configFile);
+                message = valueOf(PREFIX_KEY).getCurrentValue() + message;
+            } catch (IllegalArgumentException ignored){} // if enum didn't exist, we simply won't prefix the message
         }
+
+        return message;
     }
 
     /**
-     * Send a message to a given CommandSender.
-     * @param key The key that the message is stored as.
-     * @param sender CommandSender to send message to.
+     * Sends a message to a player using the current value contained in the language file and using the prefix defined (if any).
+     *
+     * @param commandSender The CommandSender to send the formatted message to
+     * @param args Arguments which should be used when formatting the value, see String#format(String, Object...)
      */
-    public void send(String key, CommandSender sender) {
-        send(get(key), sender);
+    public void send(CommandSender commandSender, Object... args){
+        send(commandSender, true, args);
     }
 
     /**
-     * Send a message to a given CommandSender with formatting.
-     * @param key The key that the message is stored as.
-     * @param sender CommandSender to send message to.
-     * @param format The format arguments to use with {@link java.lang.String#format(String, Object...)}.
+     * Sends a message to a commandSender using the current value contained in the language file.
+     *
+     * @param commandSender The CommandSender to send the formatted message to
+     * @param prefix Should the message be prefixed with the prefix defined?
+     * @param args Arguments which should be used when formatting the value, see String#format(String, Object...)
      */
-    public void send(String key, CommandSender sender, Object... format) {
-        send(get(key, format), sender);
-    }
-
-    private void send(Object message, CommandSender sender) {
-        if (message instanceof String) {
-            String string  = (String) message;
-            sender.sendMessage(prefix(string));
-        } else {
-            String[] list = (String[]) message;
-            sender.sendMessage(prefix(list));
-        }
+    public void send(CommandSender commandSender, boolean prefix, Object... args){
+        commandSender.sendMessage(getCurrentValue(prefix, args));
     }
 
     /**
-     * Send a raw message to a given Conversable.
-     * @param key The key that the message is stored as.
-     * @param conversable Conversable to send message to.
+     * Loads the language file and saves defaults to the file if they don't exist.
+     *
+     * @param languageFile The file to load (or save) the language values
      */
-    public void sendRaw(String key, Conversable conversable) {
-        sendRaw(get(key), conversable);
+    public static void load(File languageFile){
+        load(languageFile, null);
     }
 
     /**
-     * Send a raw message to a given Conversable with formatting.
-     * @param key The key that the message is stored as.
-     * @param conversable Conversable to send message to.
-     * @param format The format arguments to use with {@link java.lang.String#format(String, Object...)}.
+     * Loads the language file and saves defaults to the file if they don't exist.
+     *
+     * @param languageFile The file to load (or save) the language values
+     * @param header The header message to be displayed at the top of the language file, null for no header
      */
-    public void sendRaw(String key, Conversable conversable, Object... format) {
-        sendRaw(get(key, format), conversable);
-    }
-
-    private void sendRaw(Object message, Conversable conversable) {
-        if (message instanceof String) {
-            String string  = (String) message;
-            conversable.sendRawMessage(string);
-        } else {
-            String[] list = (String[]) message;
-            for (String s : list) {
-                conversable.sendRawMessage(s);
-            }
-        }
-    }
-
-    /**
-     * Get a message without formatting.
-     * Messages returned will only have color codes formatted.
-     * @param key The key that the message is stored as.
-     */
-    public Object get(String key) {
-        Object value = getValue(key);
-        if (value instanceof String) {
-            String string  = (String) value;
-            return formatColorCodes(string);
-        } else {
-            return formatColorCodes(arrayToString(value)).split(SPLIT_TOKEN);
-        }
-    }
-
-    /**
-     * Get a message with formatting.
-     * Messages returned will have color codes formatted as well as any provided arguments formatted.
-     * @param key The key that the message is stored as.
-     * @param format The format arguments to use with {@link java.lang.String#format(String, Object...)}.
-     */
-    public Object get(String key, Object... format) {
-        Object value = getValue(key);
-        if (value instanceof String) {
-            String string  = (String) value;
-            return format(string, format);
-        } else {
-            return format(arrayToString(value), format).split(SPLIT_TOKEN);
-        }
-    }
-
-    private Object getValue(String key) {
-        if (config.get(key) != null) {
-            return config.get(key);
-        } else {
-            for (String s : defaults.keySet()) {
-                if (s.equals(key)) {
-                    config.set(key, defaults.get(key));
-                    save();
-                    return defaults.get(key);
-                }
-            }
-        }
-        return null;
-    }
-
-    private void save() {
+    public static void load(File languageFile, String header){
         try {
-            config.save(configFile);
+            languageFile.createNewFile(); // make sure the file exists
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Properties defaultProperties = new Properties();
+        for (Messenger messenger : values()){
+            defaultProperties.setProperty(getKey(messenger), messenger.getDefaultValue());
+        }
+
+        Properties properties = new Properties(defaultProperties);
+        try {
+            properties.load(new FileInputStream(languageFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        for (Messenger messenger : values()){
+            messenger.currentValue = ChatColor.translateAlternateColorCodes('&', properties.getProperty(getKey(messenger)));
+        }
+
+        try {
+            properties.store(new FileOutputStream(languageFile), header);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void testDuplicates() {
-        // Run unique test on keys
-        Field[] declaredFields = this.getClass().getDeclaredFields();
-        List<String> keys = new ArrayList<String>();
-        for (Field field : declaredFields) {
-            if (Modifier.isFinal(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) {
-                if (field.getType().getName().equals("java.lang.String")) {
-                    try {
-                        String value = (String) field.get(field);
-                        if (keys.contains(value)) {
-                            System.out.println("!!WARNING: Messaging class " + field.getName() + " has duplicate key.");
-                        }
-                        keys.add(value);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
+    private static String getKey(Messenger messenger){
+        return capitalise(messenger.name().replaceAll("_", " "));
+    }
+
+    private static String capitalise(String string){
+        string = string.toLowerCase();
+
+        String[] parts = string.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < parts.length; i++){
+            String part = parts[i];
+
+            if (i == 0){
+                stringBuilder.append(part);
+            } else {
+                stringBuilder.append(Character.toUpperCase(part.charAt(0)));
+                stringBuilder.append(part.substring(1, part.length()));
             }
+
+            stringBuilder.append(" ");
         }
+
+        return stringBuilder.toString();
     }
 
-    private String format(String string, Object... format) {
-        return formatColorCodes(String.format(string, format));
-    }
-
-    private String formatColorCodes(String string) {
-        return ChatColor.translateAlternateColorCodes('&', string);
-    }
-
-    private String arrayToString(Object object) {
-        return arrayToString((List<String>) object);
-    }
-
-    private String arrayToString(List<String> list) {
-        StringBuilder builder = new StringBuilder();
-        for (String string : list) {
-            builder.append(string);
-            builder.append(SPLIT_TOKEN);
-        }
-        return builder.toString();
-    }
-
-    private String prefix(String string) {
-        if (prefix == null) {
-            return string;
-        }
-        return prefix + string;
-    }
-
-    private String[] prefix(String[] list) {
-        if (prefix == null) {
-            return list;
-        }
-        for (int i=0;i<list.length;i++) {
-            list[i] = prefix + list[i];
-        }
-        return list;
-    }
 }
